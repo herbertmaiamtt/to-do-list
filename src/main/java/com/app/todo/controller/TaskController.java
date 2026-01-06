01b506dfd4410c4e4af01b537a7fe5f4ff63817a
herbertmaiamtt <herbertmaiamtt@gmail.com>
2026-01-06 16:03:49 -0300

    feat: add getTasks endpoint and thymeleaf integration

--- src/main/java/com/app/todo/controller/TaskController.java
+++ src/main/java/com/app/todo/controller/TaskController.java
@@ -1,4 +1,30 @@
 package com.app.todo.controller;
 
+import com.app.todo.models.Task;
+import com.app.todo.services.TaskService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Controller
+//@RequestMapping("/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService){
+        this.taskService = taskService;
+    }
+
+    @GetMapping()
+    public String getTasks(Model model){
+        List<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks"; // important because of thymeleaf (search the reason later)
+    }
+
 }
